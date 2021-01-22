98345b75ccb9c2b255f67c665f32fb64adc81ebc
ange47rm <ange47rm@hotmail.com>
2021-01-22 15:35:24 +0000

    Moving back onto feature branch

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <apod/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Apod from './components/Apod.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  component: {
+    'apod' : Apod
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/Apod.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <h1>Picture of the day</h1>
+      <p> {{picOfTheDay.explanation}}</p>
+  </div>
+</template>
+
+<script>
+import { APIkey } from '../assets/APOD-API-Key';
+
+export default {
+    name: 'apod',
+    data () {
+      return {
+        picOfTheDay = undefined
+      }
+    }
+    ,
+    methods: {
+        getApod: function(){
+            fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(res => res.json())
+            .then(data => this.picOfTheDay = data)
+            },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
