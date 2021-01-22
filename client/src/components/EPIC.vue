aab7e7fd152682ae5029a05962564ac0300e3200
emcog <robin.leonard@gmail.com>
2021-01-22 21:24:25 +0000

    removed vuehello world

--- client/src/components/EPIC.vue
+++ client/src/components/EPIC.vue
@@ -17,7 +17,6 @@ export default {
     },
     methods: {
         getEpicImage: function() {
-            // fetch("https://epic.gsfc.nasa.gov/archive/natural/2015/10/31/png/epic_1b_20151031074844.png")
             fetch(this.url)
             .then(res => this.epicImage = res)
         }
