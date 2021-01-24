3c1b3b534a492d7e9248dea4ce471aed0a5dae57
Bela <belahamid@gmail.com>
2021-01-24 00:49:12 +0000

    created DateForm component

---
+++ client/src/components/DateForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>This is where the date form will appear</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name: 'date-form',
+    data() {
+        return{
+            date_entered: undefined
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
