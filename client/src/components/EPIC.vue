44120817f14444aa878ffcab460f29086ada3970
emcog <robin.leonard@gmail.com>
2021-01-25 12:47:32 +0000

    append key to recent endpoint

--- client/src/components/EPIC.vue
+++ client/src/components/EPIC.vue
@@ -3,8 +3,8 @@
         <h2>EPIC</h2><br>
         <span>with this {{this.epicImage}}</span><br>
         <span>without {{epicImage}}</span>
-        <button v-on="click">Archive</button>
-        <button v-on="click">Today</button>
+        <button v-on:click="getEpicArchivebyDate()">Archive</button>
+        <button v-on:click="getEpicRecent()">Today</button>
         <img :src="workingUrl"/>
     </div>
 </template>
@@ -19,16 +19,16 @@ export default {
         return {
             epicImage: undefined,
             testUrl: "https://api.nasa.gov/EPIC/archive/natural/2019/05/30/jpg/epic_1b_20190530011359.jpg?api_key=",
-            epicArchiveEndPoint: "https://api.nasa.gov/EPIC/archive/natural",
-            epicMostRecentEndPoint: "https://api.nasa.gov/EPIC/api/natural",
+            epicArchiveEndPoint: "https://api.nasa.gov/EPIC/archive/natural/?api_key=",
+            epicMostRecentEndPoint: "https://api.nasa.gov/EPIC/api/natural/?api_key=",
             workingUrl: "",
             inputDate:""
         }
     },
     methods: {
-        addApiKeyToUrl: function () {
-            return this.workingUrl =  this.testUrl + key.nasa;
-        },
+        // addApiKeyToUrl: function () {
+        //     return this.workingUrl =  this.testUrl + key.nasa;
+        // },
 
         addApiKeyToEndPoint: function(endPoint) {
             return this.workingUrl =  endPoint + key.nasa;
@@ -42,11 +42,17 @@ export default {
             .then(console.log(this.epicImage));
         },
 
+        getEpicRecent: function() {
+            this.addApiKeyToEndPoint(this.epicMostRecentEndPoint);
+            console.log(this.workingUrl);
+        },
 
-        getEpicJsonbyDate: function() {
-            this.addApiKeyToUrl()
-            inputDate = '2018-08-12'
-            fetch(this.workingUrl)
+        getEpicArchivebyDate: function() {
+            this.addApiKeyToEndPoint(this.epicArchiveEndPoint);
+            console.log(this.workingUrl);
+
+            // inputDate = '2018-08-12'
+            // fetch(this.workingUrl)
         },
 
         // query json 
@@ -54,9 +60,9 @@ export default {
             return arr.findIndex(el => el[(key)].indexOf(query) !== -1 )
             }
     },
-    mounted() {
-        this.getEpicImage()
-    }
+    // mounted() {
+        // this.getEpicImage()
+    // }
 
 }
 </script>
