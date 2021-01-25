9b030f7bb7df3b9eaa7761f8225cc868ba35779c
Richard Bodsworth <richardbodsworth@gmail.com>
2021-01-25 10:29:36 +0000

    fixed date interpolation error

--- client/src/components/MarsDateItem.vue
+++ client/src/components/MarsDateItem.vue
@@ -2,7 +2,7 @@
     <div>
         <form v-on:submit.prevent="handleDateSubmit">
             <label for="image_date">Select a date</label>
-            <input type="date" id="image_date" name="image_date" v-model="date_selected">
+            <input type="date" id="image_date" name="image_date" v-model="selected_date">
             <input type="submit">
         </form> 
 </template>
@@ -10,21 +10,26 @@
 <script>
 
 import { APIkey } from '@/assets/MARS_API_KEY'
+import { eventBus } from '../main.js';
 
 export default {
     name: 'mars-date-item',
     data () {
         return {
-            selectedDate: null 
+            selected_date: undefined,
+            datePic: undefined
 
         }
     },
 
     methods: {
       getPhotos: function(){
-      fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${APIkey}`)
+      fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${this.selected_date}&api_key=${APIkey}`)
       .then( res => res.json())
-      .then(data => this.selectedDate = data)
+      .then(data => this.datePic = data)
+
+    eventBus.$emit("selected-date", this.datePic)
+
 
 }}}
 
